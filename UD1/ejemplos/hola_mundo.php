9c37a1faa1ff1e3468d3369d55ba8ec4a62cb784
Samuel L.C <slcardoso.docente@gmail.com>
2024-10-05 11:19:13 +0200

    Primer ejemplo

---
+++ UD1/ejemplos/hola_mundo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola mundo</title>
+</head>
+<body>
+    <h1>Hola soy HTML</h1>
+    <?php
+        echo "<h1 style=\"color:red;\">Hola soy PHP</h1>";
+    ?>
+</body>
+</html>
