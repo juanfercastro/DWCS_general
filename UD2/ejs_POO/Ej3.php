99463d2be7bb968d46bd728c215a4b990e1be67f
Juanfercastro <jmanuelfercas@gmail.com>
2024-10-16 08:44:22 +0200

    variedad

---
+++ UD2/ejs_POO/Ej3.php
@@ -0,0 +1,139 @@
+<?php
+class Direccion{
+    private string $calle;
+    private string $ciudad;
+    private int $codigoPostal;
+
+    public function __construct(string $calle, string $ciudad, int $codigoPost)
+    {
+        $this->calle = $calle;
+        $this->ciudad = $ciudad;
+        $this->codigoPostal = $codigoPost;
+    }
+    
+    /**
+     * Get the value of calle
+     */ 
+    public function getCalle()
+    {
+        return $this->calle;
+    }
+
+    /**
+     * Set the value of calle
+     *
+     * @return  self
+     */ 
+    public function setCalle($calle)
+    {
+        $this->calle = $calle;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of ciudad
+     */ 
+    public function getCiudad()
+    {
+        return $this->ciudad;
+    }
+
+    /**
+     * Set the value of ciudad
+     *
+     * @return  self
+     */ 
+    public function setCiudad($ciudad)
+    {
+        $this->ciudad = $ciudad;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of codigoPostal
+     */ 
+    public function getCodigoPostal()
+    {
+        return $this->codigoPostal;
+    }
+
+    /**
+     * Set the value of codigoPostal
+     *
+     * @return  self
+     */ 
+    public function setCodigoPostal($codigoPostal)
+    {
+        $this->codigoPostal = $codigoPostal;
+
+        return $this;
+    }
+}
+
+class Persona{
+    private string $nombre;
+    private int $edad;
+    private Direccion $direccion;
+
+    public function __construct(string $nombre, int $edad, )
+    {
+        $this->nombre = $nombre;
+        $this->edad = $edad;
+    }
+
+    /**
+     * Get the value of nombre
+     */ 
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     *
+     * @return  self
+     */ 
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of edad
+     */ 
+    public function getEdad()
+    {
+        return $this->edad;
+    }
+
+    /**
+     * Set the value of edad
+     *
+     * @return  self
+     */ 
+    public function setEdad($edad)
+    {
+        if ($edad<0) {
+            return "La edad debe ser un número positivo";
+        }else{
+            $this->edad = $edad;
+
+            return $this;
+        }
+    }
+
+    public function esMayorDeEdad(){
+        if ($this->edad >= 18) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+}
+?>
