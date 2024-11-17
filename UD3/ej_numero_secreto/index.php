9dbad738ffd98e84948babfe848b18592664f06e
juanfercastro <jmanuelfercas@gmail.com>
2024-11-17 17:58:58 +0100

    inicio numero

--- UD3/ej_numero_secreto/index.php
+++ UD3/ej_numero_secreto/index.php
@@ -1,3 +1,18 @@
+<?php
+$rand = rand(1, 1000);
+
+if (isset($_POST['adivinar'])) {
+    $numero = $_POST['numero'];
+    if ($numero > $rand) {
+        echo "El número es menor";
+    }else if ($numero < $rand) {
+        echo "El número es mayor";
+    }else{
+        die("Has acertado!");
+    }
+}
+echo $rand;
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,5 +23,10 @@
 </head>
 <body>
     <h1>Bienvenido al número secreto</h1>
+    <form action="" method="post">
+        <label for="numero">Número: </label>
+        <input type="text" name="numero" id="numero">
+        <input type="submit" value="Adivinar" name="adivinar">
+    </form>
 </body>
 </html>
